357a945ae5ff58a6be628fd212cce0fa563c2d9b
Erik Slagter <erik@openpli.org>
2017-02-09 16:42:46 +0100

    Add support for bcm4444s (VU+ Ultimo4k).

--- main.c
+++ main.c
@@ -231,6 +231,10 @@ int main(int argc, char **argv)
 				{
 					stb_type = BRCM_ARM;
 				}
+				else if (strstr(buf,"7444s"))
+				{
+					stb_type = BRCM_ARM;
+				}
 				else if (strstr(buf,"7252"))
 				{
 					stb_type = BRCM_ARM;
