84cdfca67fd697079d148c50f97ff384d8334049
technl <tech@openpli.org>
2017-04-01 13:56:57 +0200

    Add support for bcm7252s (Xsarius Galaxy4k).

--- main.c
+++ main.c
@@ -239,6 +239,10 @@ int main(int argc, char **argv)
 				{
 					stb_type = BRCM_ARM;
 				}
+                                else if (strstr(buf,"7252s"))
+                                {
+                                        stb_type = BRCM_ARM;
+                                }
 				else if (strstr(buf,"7251"))
 				{
 					stb_type = BRCM7439;
