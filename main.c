c0d7603985e43c94ca17b6a1433756c4e31ebd4e
Persian Professionals <persianpros@yahoo.com>
2018-02-04 14:46:20 +0330

    Add the missing "BRCM7439"

--- main.c
+++ main.c
@@ -83,7 +83,7 @@ void fast_resize(const unsigned char *source, unsigned char *dest, int xsource, 
 void (*resize)(const unsigned char *source, unsigned char *dest, int xsource, int ysource, int xdest, int ydest, int colors);
 void combine(unsigned char *output, const unsigned char *video, const unsigned char *osd, int vleft, int vtop, int vwidth, int vheight, int xres, int yres);
 
-static enum {UNKNOWN, PALLAS, VULCAN, WETEKPLAY, XILLEON, BRCM_ARM, BRCM7400, BRCM7401, BRCM7405, BRCM7325, BRCM7335, BRCM7358, BRCM7362, BRCM73625, BRCM7241, BRCM7346, BRCM7356, BRCM73565, BRCM7424, BRCM7425, BRCM7435, BRCM7552, BRCM75845, BRCM7584, BRCM7581, BRCM7583, HISIL_ARM} stb_type = UNKNOWN;
+static enum {UNKNOWN, PALLAS, VULCAN, WETEKPLAY, XILLEON, BRCM_ARM, BRCM7400, BRCM7401, BRCM7405, BRCM7325, BRCM7335, BRCM7358, BRCM7362, BRCM73625, BRCM7241, BRCM7346, BRCM7356, BRCM73565, BRCM7424, BRCM7425, BRCM7435, BRCM7552, BRCM75845, BRCM7584, BRCM7581, BRCM7583, BRCM7439, HISIL_ARM} stb_type = UNKNOWN;
 
 
 static int chr_luma_stride = 0x40;
@@ -402,6 +402,12 @@ int main(int argc, char **argv)
 			registeroffset = 0x10600000;
 			chr_luma_stride = 0x80;
 			chr_luma_register_offset = 0x3c;
+			mem2memdma_register = 0;
+			break;
+		case BRCM7439:
+			registeroffset = 0xf0600000;
+			chr_luma_stride = 0x80;
+			chr_luma_register_offset = 0x34;
 			mem2memdma_register = 0;
 			break;
 		default:
@@ -880,7 +886,7 @@ void getvideo(unsigned char *video, int *xres, int *yres)
 		int adr, adr2, ofs, ofs2, offset, pageoffset;
 		int xtmp,xsub,ytmp,t2,dat1;
 
-		if (stb_type == BRCM73565 || stb_type == BRCM73625 || stb_type == BRCM75845)
+		if (stb_type == BRCM73565 || stb_type == BRCM73625 || stb_type == BRCM75845 || stb_type == BRCM7439)
 		{
 			ofs = data[chr_luma_register_offset + 24] << 4; /* luma lines */
 			ofs2 = data[chr_luma_register_offset + 28] << 4; /* chroma lines */
